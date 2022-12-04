4b5935ecc0487873d21bd33e4b76c882bd961f75
jaivikppatel <jaivikppatel@gmail.com>
2022-12-04 18:53:44 -0500

    Added Decrypt Needs Work

--- EncryptorDriver.java
+++ EncryptorDriver.java
@@ -3,6 +3,7 @@ import FileInterfacePackage.RedFileInterface;
 import FileInterfacePackage.BlackFileInterface;
 import P2PInterfacePackage.*;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class EncryptorDriver {
@@ -44,10 +45,65 @@ public class EncryptorDriver {
             case 2 -> decryptOption();
             default -> System.exit(0);
         }
+
+        mainMenu();
+    }
+
+    static void mainMenu(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("MAIN MENU");
+        System.out.println("1 --> Encrypt");
+        System.out.println("2 --> Decrypt");
+        System.out.println("Other --> Exit");
+        System.out.print("Choice? ");
+
+        int choice = scan.nextInt();
+
+        switch (choice) {
+            case 1 -> encryptOption();
+            case 2 -> decryptOption();
+            default -> System.exit(0);
+        }
     }
 
     static void encryptOption(){
         audit.FreeText(teamName, "Encryption Option Select!", false);
+        Scanner scan = new Scanner(System.in);
+        EncryptorFacade facade = EncryptorFacade.Instance();
+        RedFileInterface rfi  = new RedFileInterface();
+        BlackFileInterface bfi = new BlackFileInterface();
+
+        System.out.print("File to Encrypt: ");
+        rfi.openForRead(scan.nextLine());
+
+        System.out.print("Wheel 1 Pos: ");
+        facade.setWheel1Pos(scan.nextInt());
+        System.out.print("Wheel 2 Pos: ");
+        facade.setWheel2Pos(scan.nextInt());
+        System.out.print("Wheel 3 Pos: ");
+        facade.setWheel3Pos(scan.nextInt());
+
+        bfi.openForWrite("MyOutFile.dat");
+        char c = rfi.readInputFile();
+        while (! (rfi.EOF()) ){
+            int con = rfi.convert(c);
+            int econ = facade.encrypt(con);
+            bfi.writeToFile(econ);
+            c = rfi.readInputFile();
+        }
+        audit.SendEOK(teamName, "Encrypt Successful", false);
+        System.out.print("Wheel Positions are ");
+        System.out.print(facade.getWheel1Pos());
+        System.out.print(facade.getWheel2Pos());
+        System.out.println(facade.getWheel3Pos());
+        rfi.closeInFile();
+        bfi.closeOutFile();
+        scan.close();
+        mainMenu();
+    }
+
+    static void decryptOption(){
+        audit.FreeText(teamName, "Decryption Option Select!", false);
         Scanner scan = new Scanner(System.in);
         EncryptorFacade facade = EncryptorFacade.Instance();
         RedFileInterface rfi  = new RedFileInterface();
@@ -60,25 +116,29 @@ public class EncryptorDriver {
         System.out.print("Wheel 3 Pos: ");
         facade.setWheel3Pos(scan.nextInt());
 
-        System.out.print("File to Encrypt: ");
-        rfi.openForRead(scan.nextLine());
-        bfi.openForWrite("MyOutFile.dat");
-        char c = rfi.readInputFile();
-        while (! (rfi.EOF()) ){
-            int con = rfi.convert(c);
-            int econ = facade.encrypt(con);
-            bfi.writeToFile(econ);
-            c = rfi.readInputFile();
+        bfi.openForRead("MyOutFile.dat");
+        int i = bfi.readFromFile();
+        while (bfi.EOF() == false){
+            rfi.addToBuf(i);
+            i = bfi.readFromFile();
         }
-        audit.SendEOK(teamName, "Encrypt Successful", false);
-        rfi.closeInFile();
-        bfi.closeOutFile();
-        scan.close();
-    }
+        bfi.closeInFile();
 
-    static void decryptOption(){
-        audit.FreeText(teamName, "Decryption Option Select!", false);
-        Scanner scan = new Scanner(System.in);
+        rfi.openForWrite("MyOutFile.txt");
 
+        while (rfi.emptyBuf() == false){
+            int j = rfi.removeFromBuf();
+            j = facade.decrypt(j);
+            rfi.addToRevBuf(j);
+        }
+
+        char ch;
+        while (rfi.emptyRevBuf() == false){
+            int k = rfi.removeFromRevBuf();
+            ch = rfi.unConvert(k);
+            rfi.writeToFile(ch);
+        }
+        audit.SendDOK(teamName, "Decryption SuccessFul!", false);
+        mainMenu();
     }
 }
